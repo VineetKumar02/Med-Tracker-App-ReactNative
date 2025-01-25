c75293452975eebc4c70ce9c266a9486b3cc8f42
vineetkumar02 <vineethero02@gmail.com>
2025-01-25 14:25:11 +0530

    Created welcome page and tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+        }}>
+            <Tabs.Screen name="index"
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen name="AddNew"
+                options={{
+                    tabBarLabel: 'Add',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="plus-square" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen name="Profile"
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="user-alt" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
+    )
 }
---
+++ app/login/index.tsx
@@ -0,0 +1,85 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Colors from '@/constant/Colors'
+import { useRouter } from 'expo-router'
+
+export default function LoginPage() {
+
+    const router = useRouter();
+
+    return (
+        <View style={styles?.container}>
+            <View>
+                <Image source={require('./../../assets/images/login.png')} style={styles?.image} />
+            </View>
+
+            <View style={styles?.bottomContainer}>
+                <Text style={styles?.headerText}>Stay on Track, Stay Heathy</Text>
+                <Text style={styles?.headerSubText}>Track your meds, take control of your health. Stay consistent, stay confident</Text>
+                <TouchableOpacity style={styles?.button} onPress={() => router.push('./login/signIn')}>
+                    <Text style={styles?.btnText}>Continue</Text>
+                </TouchableOpacity>
+                <Text style={{ color: Colors.WHITE }}>Note: By Clicking Continue button, you will agree to our terms and conditions</Text>
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+    },
+    imageContainer: {
+        height: '100%',
+        width: '100%',
+        flex: 1,
+    },
+    image: {
+        width: 210,
+        height: 450,
+        borderRadius: 23,
+        resizeMode: 'cover',
+    },
+    bottomContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        // justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        padding: 30,
+        backgroundColor: Colors.PRIMARY,
+        width: '100%',
+        flex: 1,
+    },
+    headerText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+    },
+    headerSubText: {
+        fontSize: 17,
+        color: 'white',
+        textAlign: 'center',
+    },
+    button: {
+        width: '100%',
+        height: 50,
+        backgroundColor: Colors.WHITE,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    btnText: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: Colors.BLACK,
+    }
+})
